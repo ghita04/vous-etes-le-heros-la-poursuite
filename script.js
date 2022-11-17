56a041a48f858ead7898263b3be75c5e5fec8111
ghita04 <alaouighita58@gmail.com>
2022-11-16 20:11:00 -0500

    debug

--- script.js
+++ script.js
@@ -3,117 +3,97 @@
   text: "Vous vous réveillez sur la banquette arrière de la voiture d'un inconnu. Vous ne vous souvenez de rien et vous vous sentez en danger. Que faite-vous ?",
   img: "",
 };
-
 let attendre = {
   subtitle: "Arrêt à la station-service",
   text: "Le chauffeur inconnu s'arrête à une station-service assez isolée. Il jette un petit coup d'oeil vers vous à l'aide du rétroviseur pour vérifier que vous êtes toujours inconscient. Il sort son téléphone et débarre la porte afin d'aller passer un coup de fil à l'extérieur du véhicule. Vous en profitez pour vous évader en douceur, mais le bruit de vos clés ont attirés l'attention de l'inconnu et il se dirige vers vous. Vous vous enfuyez.",
   img: "",
 };
-
 let ouvrir = {
   subtitle: "Course contre la montre",
   text: "Vous attendez patiemment que la lumière vire au rouge pour ouvrir rapidement la porte passagère afin de vous précipiter dehors. L'inconnu s'en aperçoit et va se garer rapidement avant de vous poursuivre à la course.",
   img: "",
 };
-
 let attaquer = {
   subtitle: "L'accident",
   text: "Sous l'adrénaline et la peur, vous décidez d'essayer d'étrangler l'inconnu à une lumière rouge afin de pouvoir vous enfuir. Malheureusement, l'inconnu appuie malencontreusement sur l'accélérateur sous le choc et fonce droit dans une borne électrique qui explose et qui, par le fait même, met la voiture en feu. L'inconnu et vous mourrez dans cet accident.",
   img: "",
 };
-
 let chapter2 = {
   subtitle: "Coincé entre deux chemins",
   text: "Vous êtes à bout de souffle et vous vous retrouvez à choisir entre deux chemins pour vous sauver. L'inconnu vous a presque rattrapé, il faut se décider vite ! Quel chemin choissisez-vous ?",
   img: "",
 };
-
 let foret = {
   subtitle: "Promenade dans les bois",
   text: "Vous décidez de vous enfoncer dans les profondeurs de la forêt avec espoir de semer l'inconnu. Malheureusement, sous l'empressement, vous vous enfargez sur une branche qui est tombée d'un arbre massif. Le malfaiteur vous rattrape. Vous tentez de vous débattre, mais il est beaucoup trop fort pour vous. Vous cessez de résister et l'inconnu vous soulève sur son épaule comme un sac à patate. Durant le trajet pour revenir vers la voiture, vous réussissez cependant à voler votre téléphone qu'il vous avait pris et le cacher.",
   img: "",
 };
-
 riviere = {
   subtitle: "Un échappatoire rafraichissant",
   text: "Vous plongez dans la rivière et l'inconnu reste sur la berge à vous observer. Il ne vous suit plus et rebrousse chemin.",
   img: "",
 };
-
 let chapter3 = {
   subtitle: "Chute vertigineuse",
   text: "Vous pensez être enfin libre, mais vous commencez à entendre un vrombissement qui ressemble au bruit d'une énorme chute d'eau. Vos doutes se confirment lorsque vous vous sentez tiré vers l'avant. Que faites-vous ?",
   img: "",
 };
-
 let nager = {
   subtitle: "Choc irréversible",
   text: "Vous paniquez et décidez d'essayer de nager à contre-courant. Cependant, la force est bien plus grande que vos efforts et vous finissez par vous épuisez à tel point que vous ne remarquez pas le rocher juste devant vous. Vous vous cognez la tête et perdez connaissance. Vous finissez éventuellement par manquer d'air. ",
   img: "",
 };
-
 let rienfaire = {
   subtitle: "L'abandon est finalement gagnant",
   text: "Vous choisissez le choix de la chance et restez complètement mou en vous laissant porter par le courant. La chance est de votre côté, car vous vous en sortez indemne.",
   img: "",
 };
-
 let chapter4 = {
   subtitle: "Atterissage en terme ferme abrupte",
   text: "Vous vous dirigez vers la berge en nageant afin de sortir de la rivière. À peine sorti de l'eau, vous entendez soudain un bruit de froissement qui vient du buisson juste à côté. Que faites-vous ?",
   img: "",
 };
-
 let cacher = {
   subtitle: "Et si on jouait à cache-cache ?",
   text: "Vous vous dirigez en vitesse à la direction opposée pour vous cacher derrière un grand arbre qui se trouvait non loin de là. Du coin de l'oeil, vous apercevez un autre inconnu qui semblait parler au téléphone avec le malfaiteur qui vous a enlevé et vous chercher d'un air frustré. Vous commencez à paniquer à l'idée qu'il vous voit. Par chance, il finit par s'en aller et vous restez en vie.",
   img: "",
 };
-
 let immobile = {
   subtitle: "Faux espoir",
   text: "Vous vous dîtes que c'est quelqu'un du coin qui pourra vous sortir de ce pétrin et qui vous aidera à rentrer chez vous. À l'aperçu de l'individu, vous vous apercevez directement qu'il a le même regard que votre malfaiteur. Vous comprenez que c'est un de ses complices et qu'on vous a retrouvé. Il n'y a plus aucun échappatoire.",
   img: "",
 };
-
 let chapter5 = {
   subtitle: "Orientation en territoire inconnu",
   text: "Il commence à se faire tard et vous avez hâte de rentrer chez vous après toutes ces mésaventures. Mais comment vous orienter dans cet endroit que vous ne connaissez pas ? Avez-vous un outil qui pourrait vous aider",
   img: "",
 };
-
 let oui = {
   subtitle: "Google Map entre dans la partie",
   text: "Vous vous rappelez que vous aviez récupérer votre téléphone à l'inconnu. Vous ouvrez directement la carte et vous mettez en marcher vers la route la plus proche.",
   img: "",
 };
-
 let non = {
   subtitle: "Allons-y à la chance..",
   text: "Vous essayez d'avancer à l'aveugle, mais vous finissez par vous perdre et tourner en rond. Vous mourrez de fatigue et de faim.",
   img: "",
 };
-
 let chapter6 = {
   subtitle: "Proche du but",
   text: "Après de longues heures de marche, vous entendez enfin le bruit de l'autoroute. Vous vous sentez soulagé et vous vous en rapprocher. Deux choix s'offrent à vous.",
   img: "",
 };
-
 let marcher = {
   subtitle: "Enfin chez-soi ! ",
   text: "Vous ne voulez plus prendre de risque et décidez de marcher jusqu'à chez vous en faisant quelques arrêts pour vous nourrir et vous reposer. Après quelques heures, vous réussisez enfin à atteindre votre quartier et vous pouvez maintenant rentrer chez vous.",
   img: "",
 };
-
 let pouce = {
   subtitle: "Retour à la case départ",
   text: "Vous venez de marcher pour trouver cette route et vous vous sentez beaucoup trop fatigué pour recommencer. Vous décidez donc de vous mettre sur le côté et faire du pouce. Une voiture finit par s'arrêter et vous embarque volontier. Tout semblait se passer à merveille, lorsque, soudain, votre sauveur s'arrête et pose sa main sur votre cuisse avec un regard bien plus que clair. Vous êtes coincé de nouveau.",
   img: "",
 };
-
 Si le code bug encore encore et met undefined
-
 */
 
 let chapterObj = {
@@ -156,7 +136,7 @@ let chapterObj = {
     options: [
       {
         optionText: "Continuer",
-        action: 'goToChapter("chapter2")',
+        action: 'telephonetrue()',
       },
     ],
   },
@@ -204,6 +184,7 @@ let chapterObj = {
     subtitle: "Un échappatoire rafraichissant",
     text: "Vous plongez dans la rivière et l'inconnu reste sur la berge à vous observer. Il ne vous suit plus et rebrousse chemin.",
     img: "assets/images/riviere.jpg",
+    video: "assets/images/vague.mp4",
     options: [
       {
         optionText: "Continuer",
@@ -289,16 +270,12 @@ let chapterObj = {
 
   chapter5: {
     subtitle: "Orientation en territoire inconnu",
-    text: "Il commence à se faire tard et vous avez hâte de rentrer chez vous après toutes ces mésaventures. Mais comment vous orienter dans cet endroit que vous ne connaissez pas ? Avez-vous un outil qui pourrait vous aider",
+    text: "Il commence à se faire tard et vous avez hâte de rentrer chez vous après toutes ces mésaventures. Mais comment vous orienter dans cet endroit que vous ne connaissez pas ? Avez-vous un téléphone qui pourrait vous aider",
     img: "assets/images/boussole.jpg",
     options: [
       {
-        optionText: "Avez-vous votre téléphone ?",
-        action: 'goToChapter("oui")',
-      },
-      {
-        optionText: "Vous décidez d'y aller à l'aveugle",
-        action: 'goToChapter("non")',
+        optionText: "Vérifier",
+        action: 'telephoneStatus()',
       },
     ],
   },
@@ -373,7 +350,7 @@ function goToChapter(chapterName) {
   document.querySelector(".titre").innerHTML =
     chapterObj[chapterName]["subtitle"];
   document.querySelector(".texte").innerHTML = chapterObj[chapterName]["text"];
-  document.querySelector("img").src = chapterObj[chapterName].img;
+  document.querySelector("img").src = `<img src="${chaptersObj[chapterName].img}"/>`;
 
   document.querySelector(".arrButton").innerHTML = "";
   for (i in chapterObj[chapterName].options) {
@@ -387,7 +364,14 @@ function goToChapter(chapterName) {
     const parent = document.querySelector(".arrButton");
     parent.appendChild(btn);
   }
+  
+  if (chaptersObj[chapterName].video) {
+    document.querySelector("img").src = `<video src="${chaptersObj[chapterName].video}" muted autoplay loop></video>`;
+} else {
+   document.querySelector("img").src = `<img src="${chaptersObj[chapterName].img}"/>`;
 }
+  
+  localStorage.setItem("chaptersObj", "chapterName");
 goToChapter("chapter1");
 
 let telephone = false;
@@ -401,12 +385,6 @@ function telephoneStatus() {
   if (telephone == true) {
     goToChapter("oui");
   }
-  if (telephone == false) {
+  else (telephone == false) {
     goToChapter("non");
-  }
-}
-
-function telephonefalse() {
-  telephone = false;
-  goToChapter("non");
-}
+  }};
